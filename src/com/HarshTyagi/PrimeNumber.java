18accb357e56c1537a2c6376ee49c128e8ba9b72
Harsh Tyagi <harshtyagi9897u@gmail.com>
2022-12-21 12:09:51 +0530

    Some basic problem solution

--- src/com/HarshTyagi/PrimeNumber.java
+++ src/com/HarshTyagi/PrimeNumber.java
@@ -2,6 +2,17 @@ package com.HarshTyagi;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-
+        System.out.println(isPrime(3));
+    }
+    public static boolean isPrime(int n){
+     if(n==1){
+         return true;
+     }
+     for(int i=2;i*i<=n;i++){
+         if(n%i==0){
+             return false;
+         }
+     }
+     return true;
     }
 }
