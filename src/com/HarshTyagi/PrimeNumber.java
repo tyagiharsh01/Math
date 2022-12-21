106da40082f45e3e8eb02b50f4a1ac97cb8b9835
Harsh Tyagi <harshtyagi9897u@gmail.com>
2022-12-21 12:25:32 +0530

    Adding effective solution

--- src/com/HarshTyagi/PrimeNumber.java
+++ src/com/HarshTyagi/PrimeNumber.java
@@ -2,7 +2,7 @@ package com.HarshTyagi;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println(isPrime(3));
+        System.out.println(isprime(1728971));
     }
     public static boolean isPrime(int n){
      if(n==1){
@@ -15,4 +15,20 @@ public class PrimeNumber {
      }
      return true;
     }
+    // more efficent solution
+    public static boolean isprime(int n) {
+        if (n == 1)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        }
+        return true;
+    }
+
 }
